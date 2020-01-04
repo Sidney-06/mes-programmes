a61aa74684b76d34f3f80e0d0fbd93653ccfc6a6
Sidney-06 <59512553+Sidney-06@users.noreply.github.com>
2020-01-04 13:40:15 -0500

    Update ma premiere fenetre.py

--- ma premiere fenetre.py
+++ ma premiere fenetre.py
@@ -8,11 +8,11 @@ window.title("Pierre, Papier, Ciseaux !!!")
 window.geometry("600x600")
 window.maxsize()
 window.iconbitmap("51V-YSNt-iL.ico")
-window.config(background='#258521')
+window.config(background = '#258521')
 
 # texte
-label_title = Label(window, text="Bonjour Christophe !!!", font=("courrier", 40), bg='red', fg='black')
-label_title.pack(expand=YES)
+label_title = Label(window, text = "Bonjour Christophe !!!", font = ("courrier", 40), bg = 'red', fg = 'black')
+label_title.pack(expand = YES)
 
 # afficher
 window.mainloop()

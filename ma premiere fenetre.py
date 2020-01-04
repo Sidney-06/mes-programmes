a199898fd2a7be380516d2e26cc8a6617acdc409
Sidney-06 <59512553+Sidney-06@users.noreply.github.com>
2020-01-04 14:33:43 -0400

    Add files via upload

---
+++ ma premiere fenetre.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+# ma fenetre
+window = Tk()
+
+# caractéristique de la fenetre
+window.title("Pierre, Papier, Ciseaux !!!")
+window.geometry("600x600")
+window.maxsize()
+window.iconbitmap("51V-YSNt-iL.ico")
+window.config(background='#258521')
+
+# texte
+label_title = Label(window, text="Bonjour Christophe !!!", font=("courrier", 40), bg='red', fg='black')
+label_title.pack(expand=YES)
+
+# afficher
+window.mainloop()
